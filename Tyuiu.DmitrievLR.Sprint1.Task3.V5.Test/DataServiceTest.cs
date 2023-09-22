fc042c5c8c1582fc269af32881e71d1f615170b8
DmitrievLeonid <Tyuiu.DmitrievLR@outlook.com>
2023-09-22 17:42:52 +0500

    Task3

---
+++ Tyuiu.DmitrievLR.Sprint1.Task3.V5.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.DmitrievLR.Sprint1.Task3.V5.Lib;
+
+namespace Tyuiu.DmitrievLR.Sprint1.Task3.V5.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+
+            int Scale = 120;
+            double distance = 3.5;
+            int wait = 420;
+
+            var res = ds.DistanceLength(Scale, distance);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.DmitrievLR.Sprint1.Task3.V5/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.DmitrievLR.Sprint1.Task3.V5.Lib;
+
+/*Написать программу, которая запрашивает у пользователя исходные данные,
+ * выполняет указанные расчёты и печатает результат на экране.
+ * Написать программу вычисления расстояния между населенными пунктами, изображенными на карте.
+ */
+
+namespace Tyuiu.DmitrievLR.Sprint1.Task3.V5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DataService ds = new DataService();
+            //Сокращаем ссылку на DataService
+
+            Console.Title = " Спринт 1 | Выполнил Дмитриев Л. Р.  | ИИПб-23-1 ";
+
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* Спринт #1                                                              *");
+            Console.WriteLine("* Тема: Алгоритмы линейной структуры                                     *");
+            Console.WriteLine("* Задание #3                                                             *");
+            Console.WriteLine("* Вариант: #5                                                            *");
+            Console.WriteLine("* Выполнил: Дмитриев Леонид Романович | ИИПб-23-1                        *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                               *");
+            Console.WriteLine("* Написать программу вычисления расстояния между населенными пунктами,   *");
+            Console.WriteLine("* изображенными на карте.                                                *");
+            Console.WriteLine("*                                                                        *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+            Console.WriteLine("**************************************************************************");
+            /*
+             *   Console.WriteLine("* РЕЗУЛЬТАТ:                                                             *");
+                 Console.WriteLine("**************************************************************************");
+             */
+            Console.WriteLine("Введите масштаб карты (количество км. в одном см.): ");
+            double Scale = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите расстояние между точками, изображающими населенные пункты (см) : ");
+            double distance = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                             *");
+            Console.WriteLine("**************************************************************************");
+            var result = ds.DistanceLength(Scale, distance);
+            Console.WriteLine("Расстояние между населенными пунктами: " + result + " Км.");
+
+            Console.ReadKey();
+        }
+    }
+}
