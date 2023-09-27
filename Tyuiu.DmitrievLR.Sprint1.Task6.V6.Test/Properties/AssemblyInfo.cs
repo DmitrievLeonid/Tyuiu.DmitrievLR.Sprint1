f343158279cfed3c48d9ee3c5b13f4998f3209b3
DmitrievLeonid <Tyuiu.DmitrievLR@outlook.com>
2023-09-27 19:10:11 +0500

    Task6

---
+++ Tyuiu.DmitrievLR.Sprint1.Task6.V6.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.DmitrievLR.Sprint1.Task6.V6.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.DmitrievLR.Sprint1.Task6.V6.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("cccf997e-8d22-481f-b1b8-e0385c7786e5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.DmitrievLR.Sprint1.Task6.V6/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.DmitrievLR.Sprint1.Task6.V6.Lib;
+
+namespace Tyuiu.DmitrievLR.Sprint1.Task6.V6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            //Сокращаем ссылку на DataService
+
+            Console.Title = " Спринт 1 | Выполнил Дмитриев Л. Р.  | ИИПб-23-1 ";
+
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* Спринт #1                                                              *");
+            Console.WriteLine("* Тема: Работа со строками класс String                                  *");
+            Console.WriteLine("* Задание #6                                                             *");
+            Console.WriteLine("* Вариант: #6                                                            *");
+            Console.WriteLine("* Выполнил: Дмитриев Леонид Романович | ИИПб-23-1                        *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                               *");
+            Console.WriteLine("* Написать программу: пользователь вводит текст.                         *");
+            Console.WriteLine("* Напечатать все слова, удалив из них первую букву.                      *");
+            Console.WriteLine("*                                                                        *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+            Console.WriteLine("**************************************************************************");
+            /*
+             *   Console.WriteLine("* РЕЗУЛЬТАТ:                                                             *");
+                 Console.WriteLine("**************************************************************************");
+             */
+
+            Console.WriteLine("Введите текст: ");
+            string x = Console.ReadLine();
+
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                             *");
+            Console.WriteLine("**************************************************************************");
+
+            Console.WriteLine(ds.DeleteFirstLetter(x));
+
+            Console.ReadKey();
+
+        }
+    }
+}
